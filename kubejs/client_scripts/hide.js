60014eee7a8d952a121747561956ebdef21d12c6
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2022-01-08 04:06:23 +0100

    Add Lead unification

--- kubejs/client_scripts/hide.js
+++ kubejs/client_scripts/hide.js
@@ -4,4 +4,11 @@ onEvent('jei.hide.items', (event) => {
 	event.hide('mysticalagriculture:infusion_crystal')
 	event.hide('moremekanismprocessing:cobalt_ingot')
 	event.hide('moremekanismprocessing:cobalt_nugget')
+
+	// Lead
+	event.hide('mekanism:lead_ore')
+	event.hide('mekanism:dust_lead')
+	event.hide('mekanism:ingot_lead')
+	event.hide('mekanism:nugget_lead')
+	event.hide('mekanism:block_lead')
 })
--- kubejs/server_scripts/unification.js
+++ kubejs/server_scripts/unification.js
@@ -8,6 +8,13 @@ events.listen('item.tags', (event) => {
     event.removeAllTagsFrom('mekanism:ingot_lead')
     event.removeAllTagsFrom('mekanism:nugget_lead')
     event.removeAllTagsFrom('mekanism:dust_lead')
+    event.removeAllTagsFrom('mekanism:block_lead')
+})
+
+events.listen('block.tags', (event) => {
+    // Lead
+    event.removeAllTagsFrom('mekanism:lead_ore')
+    event.removeAllTagsFrom('mekanism:block_lead')
 })
 
 events.listen('recipes', (event) => {
@@ -22,4 +29,20 @@ events.listen('recipes', (event) => {
     event.blasting('tconstruct:cobalt_ingot', 'moremekanismprocessing:dust_cobalt')
 
     // Lead
+    event.remove({ id: 'mekanism:processing/lead/ore/from_dust' })
+    event.remove({ id: 'mekanism:processing/lead/ingot/from_nuggets' })
+    event.remove({ id: 'mekanism:processing/lead/ingot/from_block' })
+    event.remove({ id: 'create:blasting/ingot_lead_compat_mekanism' })
+    event.remove({ id: 'mekanism:processing/lead/ingot/from_ore_blasting' })
+    event.remove({ id: 'mekanism:processing/lead/ingot/from_dust_blasting' })
+    event.remove({ id: 'mekanism:processing/lead/ingot/from_dust_smelting' })
+    event.remove({ id: 'create:smelting/ingot_lead_compat_mekanism' })
+    event.remove({ id: 'mekanism:processing/lead/ingot/from_ore_smelting' })
+    event.remove({ id: 'mekanism:processing/lead/nugget/from_ingot' })
+    event.remove({ id: 'create:splashing/mekanism/crushed_lead_ore' })
+    event.remove({ id: 'mekanism:processing/lead/dust/from_ingot' })
+    event.remove({ id: 'mekanism:processing/lead/dust/from_dirty_dust' })
+    event.remove({ id: 'mekanism:processing/lead/dust/from_ore' })
+    event.recipes.mekanism.enriching(Item.of('thermal:lead_dust', 2), '#forge:ores/lead')
+    event.remove({ id: 'mekanism:processing/lead/storage_blocks/from_ingots' })
 })
