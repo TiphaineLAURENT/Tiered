a3d5e525d06c6827dde68d52df0a8e6df059dc4e
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2022-01-08 23:04:04 +0100

    Add ExtraCoals and Rainbow coal tooltips

---
+++ kubejs/client_scripts/tooltips.js
@@ -0,0 +1,9 @@
+events.listen('item.tooltip', (tooltip) => {
+    tooltip.add('extracoals:lapis_coal', ['Double speed'])
+    tooltip.add('extracoals:redstone_coal', ['Quadruple speed'])
+    tooltip.add('extracoals:gold_coal', ['Octuple speed'])
+    tooltip.add('extracoals:emerald_coal', ['Double speed'])
+    tooltip.add('extracoals:diamond_coal', ['Quadruple speed'])
+
+    tooltip.add('ironfurnaces:rainbow_coal', ['Consumes 1 out of 5000 durabilities for each item smelt', 'Ideal for smelting a lot of individual items'])
+})
