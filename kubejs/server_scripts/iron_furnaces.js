5413aef0df41c2deac75fb81ed3a462dd748096c
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2022-01-08 23:03:42 +0100

    Add Rainbow coal recipe

---
+++ kubejs/server_scripts/iron_furnaces.js
@@ -0,0 +1,3 @@
+events.listen('recipes', function (event) {
+    event.shapeless('ironfurnaces:rainbow_coal', ['mysticalagradditions:inferium_coal', 'mysticalagradditions:prudentium_coal', 'mysticalagradditions:tertium_coal', 'mysticalagradditions:imperium_coal', 'mysticalagradditions:supremium_coal', 'mysticalagradditions:insanium_coal'])
+})
