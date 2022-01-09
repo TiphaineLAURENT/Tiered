6394ff461d714673087a8347296d5d21b083aa4f
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2022-01-09 17:40:17 +0100

    Add Botania custom recipes

--- kubejs/server_scripts/botania.js
+++ kubejs/server_scripts/botania.js
@@ -1,10 +1,2 @@
 events.listen('recipes', (event) => {
-    event.replaceInput({ mod: 'botania' }, 'botania:livingrock', 'integrateddynamics:crystalized_menril_block')
-    event.replaceOutput({ mod: 'botania' }, 'botania:livingrock', 'integrateddynamics:crystalized_menril_block')
-
-    event.replaceInput({ mod: 'botania' }, 'botania:mana_pearl', 'rftoolsbase:infused_diamond')
-    event.replaceOutput({ mod: 'botania' }, 'botania:mana_pearl', 'rftoolsbase:infused_diamond')
-
-    event.replaceInput({ mod: 'botania' }, 'botania:mana_pearl', 'rftoolsbase:infused_enderpearl')
-    event.replaceOutput({ mod: 'botania' }, 'botania:mana_pearl', 'rftoolsbase:infused_enderpearl')
 })
