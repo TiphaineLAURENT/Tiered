4127bedbec869d5d41908e5473a272d76ba3ad82
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2022-01-09 17:17:36 +0100

    Add Chaos seed

--- kubejs/server_scripts/mystical_agriculture.js
+++ kubejs/server_scripts/mystical_agriculture.js
@@ -2,6 +2,24 @@ events.listen('recipes', (event) => {
     // Infusion crystal
     event.remove({ input: 'mystical_agriculture:infusion_crystal' })
 
+    // Chaos nugget
+    event.shaped('draconicevolution:small_chaos_frag',
+        [
+            'EEE',
+            'E E',
+            'EEE'
+        ], { E: 'mysticalagriculture:chaos_essence' }
+    )
+
+    // Chaos Crux
+    event.shaped(item.of('kubejs:chaotic_crux', 1),
+        [
+            'ABA',
+            'BCB',
+            'ABA'
+        ], { A: 'mysticalagradditions:insanium_essence', B: 'draconicevolution:chaos_shard', C: 'draconicevolution:awakened_draconium_block' }
+    )
+
     // Ultimate nugget
     event.shaped('extendedcrafting:the_ultimate_nugget',
         [
--- kubejs/startup_scripts/blocks.js
+++ kubejs/startup_scripts/blocks.js
@@ -1,3 +1,4 @@
 events.listen('block.registry', (event) => {
     event.create('smithing_machine_block').material('iron').hardness(0.5).displayName('Smithing Machine Block')
+    event.create('chaotic_crux').material('rock').hardness(0.5).displayName('Chaotic Crux')
 })
