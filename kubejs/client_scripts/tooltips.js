a19eb6a4e719afb8fbf2d40303f4ad4186e44577
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2022-01-09 02:46:28 +0100

    Adjust MiniUtilities config

--- kubejs/client_scripts/tooltips.js
+++ kubejs/client_scripts/tooltips.js
@@ -6,4 +6,7 @@ events.listen('item.tooltip', (tooltip) => {
     tooltip.add('extracoals:diamond_coal', ['Quadruple speed'])
 
     tooltip.add('ironfurnaces:rainbow_coal', ['Consumes 1 out of 5000 durabilities for each item smelt', 'Ideal for smelting a lot of individual items'])
+
+    tooltip.add('miniutilities:solar_panel', ['Generates 4 RF/t', 'Multiplies generation every 2 panels'])
+    tooltip.add('miniutilities:lunar_panel', ['Generates 4 RF/t', 'Multiplies generation every 2 panels'])
 })
